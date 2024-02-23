609e3a823605f455f24d6189cc03723ec155a320
BabySasukeNotNaruto <98043284+BabySasukeNotNaruto@users.noreply.github.com>
2024-02-22 18:59:50 -0600

    Update DisplayCart.js

--- DisplayCart.js
+++ DisplayCart.js
@@ -1,16 +1,1 @@
-// Get the cart items from localStorage
-let cartItems = localStorage.getItem('cartItems');
-cartItems = cartItems ? JSON.parse(cartItems) : [];
 
-// Get the <ul> element where the cart items will be displayed
-const cartItemsElement = document.getElementById('cart-items');
-
-// Clear any existing items in the cart
-cartItemsElement.innerHTML = '';
-
-// Display each cart item in the <ul> element
-cartItems.forEach(item => {
-    const li = document.createElement('li');
-    li.textContent = `${item.name} - $${item.price.toFixed(2)}`;
-    cartItemsElement.appendChild(li);
-});
